8b6d1ee12a674014232f6f93855119bfd356481d
ArekSzlapa <91280448+ArekSzlapa@users.noreply.github.com>
2024-05-29 11:55:11 +0200

    fix: change type

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // prd set of keys
 
-modules.export = {
+module.export = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,

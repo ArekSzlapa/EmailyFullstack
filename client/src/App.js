47cc7bca4094cb3233b77096e633570115ce7ab9
ArekSzlapa <91280448+ArekSzlapa@users.noreply.github.com>
2024-06-10 15:26:33 +0200

    chore: add frontend server

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Henlo</h2>
+      <a href="/auth/google">Click me to login</a>
+    </div>
+  );
+}
+
+export default App;
